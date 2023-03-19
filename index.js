c90b78b90f70c8328100d2d884c3ab31c3c675fc
Josavad Villarreal <josavadjvillarreal@gmail.com>
2023-03-18 20:29:19 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+}
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+    delete employee.key;
+    return employee;
+}
+
